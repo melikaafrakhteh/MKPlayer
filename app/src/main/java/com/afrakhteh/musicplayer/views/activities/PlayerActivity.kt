504069ffd30bb1e5c788cd03be7cc2104b03d0b4
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-08-22 13:56:45 +0430

    implement player ui

--- app/src/main/java/com/afrakhteh/musicplayer/views/activities/PlayerActivity.kt
+++ app/src/main/java/com/afrakhteh/musicplayer/views/activities/PlayerActivity.kt
@@ -1,15 +1,55 @@
 package com.afrakhteh.musicplayer.views.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.afrakhteh.musicplayer.databinding.ActivityPlayerBinding
 
+
 class PlayerActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initialiseView()
+    }
 
+    private fun initialiseView() {
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        buttonClicks()
     }
+
+    private fun buttonClicks() {
+        binding.playBackIv.setOnClickListener(::backButton)
+        binding.playRewindBtnIv.setOnClickListener(::rewindButton)
+        binding.playForwardBtnIv.setOnClickListener(::forwardButton)
+        binding.playVolume.setOnClickListener(::setVolume)
+        binding.playToggleBtn.setOnClickListener(::handlePlayOrPause)
+    }
+
+    private fun handlePlayOrPause(view: View?) {
+        //pause or play music
+    }
+
+    private fun setVolume(view: View?) {
+        //set volume of music
+    }
+
+    private fun forwardButton(view: View?) {
+        //next music
+    }
+
+    private fun rewindButton(view: View?) {
+        // previous music
+    }
+
+    private fun backButton(view: View?) {
+        //back to main activity
+    }
+
 }
+
+
